1516fad9b1ca7a97d90b6729f9d3c8b92b76fb84
Linssi Colin Ruiz <linssi101@gmail.com>
2023-02-01 16:55:55 -0600

    moved axios GET and POST routes from app.jsx into index.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,12 +13,14 @@ function App() {
   }
 
   const getElements = () => {
-    axios.get('/api/element').then(response => {
-      dispatch({ type: 'SET_ELEMENTS', payload: response.data });
-    })
-      .catch(error => {
-        console.log('error with element get request', error);
-      });
+    dispatch({type: 'FETCH_ELEMENTS'});
+
+    // axios.get('/api/element').then(response => {
+    //   dispatch({ type: 'SET_ELEMENTS', payload: response.data });
+    // })
+    //   .catch(error => {
+    //     console.log('error with element get request', error);
+    //   });
   }
 
   useEffect(() => {
@@ -26,13 +28,16 @@ function App() {
   }, []);
 
   const handleClick = () => {
-    axios.post('/api/element', {newElement}).then(() => {
-      getElements();
-      setNewElement('');
-    })
-      .catch(error => {
-        console.log('error with element get request', error);
-      });
+    dispatch({type: "POST_ELEMENT", payload: newElement})
+          setNewElement('');
+
+    // axios.post('/api/element', {newElement}).then(() => {
+    //   getElements();
+    //   setNewElement('');
+    // })
+    //   .catch(error => {
+    //     console.log('error with element get request', error);
+    //   });
 
   }
 
